3355b66e2006c3006b127dc148eed01f54262e9e
songnb <songningbo520@163.com>
2021-05-11 22:26:33 +0800

    no message

---
+++ springboot-helloword/src/main/java/com/bccoder/helloword/SpringbootHellowordApplication.java
@@ -0,0 +1,12 @@
+package com.bccoder.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellowordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHellowordApplication.class, args);
+    }
+
+}
---
+++ springboot-helloword/src/test/java/com/bccoder/helloword/SpringbootHellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bccoder.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHellowordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
